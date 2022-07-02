cf42f70565ff9d1b4e8f4371b22c8eecf1b38ec4
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-02 11:01:29 -0300

    SidebarToggle implementado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'client-point-web';
+
+  ngAfterViewInit() {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+      sidebarToggle.addEventListener('click', event => {
+        event.preventDefault();
+        document.body.classList.toggle('sb-sidenav-toggled');
+      });
+    }
+  }
 }
