89cd908c4fff631b95012e57306840008b1991c5
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-09 10:05:14 -0300

    Configuracao de rotas, navegacao entre paginas e manutencao de estilo css implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerFormComponent } from './components/customer/customer-form/customer-form.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "customer-form",
+    component: CustomerFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { TemplateComponent } from './components/template/template.component';
 import { NavbarComponent } from './components/template/navbar/navbar.component';
 import { SidebarComponent } from './components/template/sidebar/sidebar.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CustomerFormComponent } from './components/customer/customer-form/customer-form.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    CustomerFormComponent
   ],
   imports: [
     BrowserModule,
